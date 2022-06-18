f68408839af91f6d985717bbbb8c520c2be36641
reza--boy <60574856+danelloptz@users.noreply.github.com>
2022-06-18 16:19:37 +0300

    Add files via upload

--- captcha/index.js
+++ captcha/index.js
@@ -1,58 +1,66 @@
-
-let ballEl, ballDown, ball;
+let ballEl, ball, ballDown;
 let check = true;
 let Cord = {
     y_top : ($(document).height() - $('#colba').height()) / 2 + $('#ball').height() / 2,
     y_bottom : $(document).height() - (($(document).height() - $('#colba').height()) / 2) - $('#ball').height() / 2
 }
 
-$('#colba').mousemove(function (el) {
-    $('#ball').mousedown(function () {
-        ballDown = true; // отмечаем, что нажат
-    });
-    $('#ball').mouseup(function () {
-        ballDown = false; // отмечаем, что отжат
-    });
-    /* Изменяем положение фигуры, когда передвигаем его мышкой.
-    В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
-    мышка находится внутри поля. */
-    if ((ballDown == true) && (el.pageY >= Cord.y_top) && (el.pageY <= Cord.y_bottom)) {
-        $('#ball').css('top', `${el.pageY - ($('#ball').height() * 1.5)}px`);
-        //console.log($('#ball'));
-    }
-    if (($('#ball')[0].offsetTop <= 30) && (check == true)) {
-        check = false;
-        alert('Вы не робот!');
-        ballDown = false;
-        console.log($('#ball'));
-    }
-    if (ballDown == false) {
-        $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
-    }
-});
 
 // mobile
-$('#colba').touchmove(function (el) {
-    $('#ball').touchstart(function () {
-        ballDown = true; // отмечаем, что нажат
-    });
-    $('#ball').touchend(function () {
-        ballDown = false; // отмечаем, что отжат
-    });
-    /* Изменяем положение фигуры, когда передвигаем его мышкой.
-    В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
-    мышка находится внутри поля. */
-    if ((ballDown == true) && (el.pageY >= Cord.y_top) && (el.pageY <= Cord.y_bottom)) {
-        $('#ball').css('top', `${el.pageY - ($('#ball').height() * 1.5)}px`);
-        //console.log($('#ball'));
-    }
-    if (($('#ball')[0].offsetTop <= 30) && (check == true)) {
-        check = false;
-        alert('Вы не робот!');
+
+function is_touch_device() {
+    if (!!('ontouchstart' in window)) {
+        ballDown = true;
+        $('#colba').bind('touchmove', function (el) {
+            $('#ball').bind('touchstart',function () {
+                ballDown = true; // отмечаем, что нажат
+            });
+            $('#ball').bind('touchend', function () {
+                ballDown = false; // отмечаем, что отжат
+            });
+            /* Изменяем положение фигуры, когда передвигаем его мышкой.
+            В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
+            мышка находится внутри поля. */
+            if ((ballDown == true)) {
+                $('#ball').css('top', `${el.changedTouches[0].pageY - ($(document).height() - $('#colba').height()) / 2 - 50}px`);
+                //console.log(el.pageY - ($('#ball').height() * 1.5));
+            }
+            if (($('#ball')[0].offsetTop <= 30) && (check == true)) {
+                check = false;
+                alert('Вы не робот!');
+                ballDown = false;
+                console.log($('#ball'));
+            }
+            if (ballDown == false) {
+                $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
+            }
+        });
+    } else {
         ballDown = false;
-        console.log($('#ball'));
+        $('#colba').mousemove(function (el) {
+            $('#ball').mousedown(function () {
+                ballDown = true; // отмечаем, что нажат
+            });
+            $('#ball').mouseup(function () {
+                ballDown = false; // отмечаем, что отжат
+            });
+            /* Изменяем положение фигуры, когда передвигаем его мышкой.
+            В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
+            мышка находится внутри поля. */
+            if ((ballDown == true) && (el.pageY >= Cord.y_top) && (el.pageY <= Cord.y_bottom)) {
+                $('#ball').css('top', `${el.pageY - ($('#ball').height() * 1.5)}px`);
+                //console.log($('#ball'));
+            }
+            if (($('#ball')[0].offsetTop <= 30) && (check == true)) {
+                check = false;
+                alert('Вы не робот!');
+                ballDown = false;
+                console.log($('#ball'));
+            }
+            if (ballDown == false) {
+                $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
+            }
+        });
     }
-    if (ballDown == false) {
-        $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
-    }
-});
+}
+is_touch_device();
