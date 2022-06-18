cc42af206669a10ad499a66d3f157e53600315c4
reza--boy <60574856+danelloptz@users.noreply.github.com>
2022-06-18 15:45:29 +0300

    Add files via upload

--- captcha/index.js
+++ captcha/index.js
@@ -30,3 +30,29 @@ $('#colba').mousemove(function (el) {
         $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
     }
 });
+
+// mobile
+$('#colba').touchmove(function (el) {
+    $('#ball').touchstart(function () {
+        ballDown = true; // отмечаем, что нажат
+    });
+    $('#ball').touchend(function () {
+        ballDown = false; // отмечаем, что отжат
+    });
+    /* Изменяем положение фигуры, когда передвигаем его мышкой.
+    В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
+    мышка находится внутри поля. */
+    if ((ballDown == true) && (el.pageY >= Cord.y_top) && (el.pageY <= Cord.y_bottom)) {
+        $('#ball').css('top', `${el.pageY - ($('#ball').height() * 1.5)}px`);
+        //console.log($('#ball'));
+    }
+    if (($('#ball')[0].offsetTop <= 30) && (check == true)) {
+        check = false;
+        alert('Вы не робот!');
+        ballDown = false;
+        console.log($('#ball'));
+    }
+    if (ballDown == false) {
+        $('#ball').css('top', `${$('#colba').height() - $('#ball').height()}px`);
+    }
+});
