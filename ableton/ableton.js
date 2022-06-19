6493ea7018339ce7298a9ed7ea7a7be90bfd0bcb
reza--boy <60574856+danelloptz@users.noreply.github.com>
2022-06-19 20:51:03 +0300

    Add files via upload

--- ableton/ableton.js
+++ ableton/ableton.js
@@ -55,3 +55,69 @@ $('#preview').on('click', function() {
     $('#rutube-video').show();
     $('#preview').hide();
 });
+
+let ballEl, ball, ballDown;
+let check = true;
+/*let Cord = {
+    y_top : ($(document).height() - $('#wrapper').height()) / 2 + $('#text')[0].offsetTop,
+    y_bottom : ($(document).height() - $('#wrapper').height()) / 2 + $('#text')[0].offsetTop + $('#colba')[0].offsetTop
+}*/
+
+
+// mobile
+$(document).ready(function() {
+    $('html').data('previous-overflow', $('html').css('overflow')).css('overflow', 'hidden');
+    $('#modal').show();
+    if (!!('ontouchstart' in window)) {
+        ballDown = true;
+        $('#modal-colba').bind('touchmove', function (el) {
+            $('#modal-ball').bind('touchstart',function () {
+                ballDown = true; // отмечаем, что нажат
+            });
+            $('#modal-ball').bind('touchend', function () {
+                ballDown = false; // отмечаем, что отжат
+            });
+            /* Изменяем положение фигуры, когда передвигаем его мышкой.
+            В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
+            мышка находится внутри поля. */
+            if ((ballDown == true) && (el.changedTouches[0].pageY <= ($(window).height() - $('#modal-wrapper').height()) / 2 + $('#modal-colba')[0].offsetTop)) {
+                $('#modal-ball').css('top', `${el.changedTouches[0].pageY - ($(window).height() - $('#modal-colba').height()) / 2 - 50}px`);
+            }
+            if (($('#modal-ball')[0].offsetTop <= 30) && (check == true)) {
+                $('#modal').hide();
+                $('html').css('overflow', $('html').data('previous-overflow'));
+                check = false;
+                ballDown = false;
+            }
+            if (ballDown == false) {
+                $('#modal-ball').css('top', `${$('#modal-colba').height() - $('#modal-ball').height()}px`);
+            }
+        });
+    } else {
+        ballDown = false;
+        $('#modal').show();
+        $('#modal-colba').mousemove(function (el) {
+            $('#modal-ball').mousedown(function () {
+                ballDown = true; // отмечаем, что нажат
+            });
+            $('#modal-ball').mouseup(function () {
+                ballDown = false; // отмечаем, что отжат
+            });
+            /* Изменяем положение фигуры, когда передвигаем его мышкой.
+            В условии проверяем, что элемент "зацеплен" (зажат мышкой) и
+            мышка находится внутри поля. */
+            if ((ballDown == true) && (el.pageY <= ($(window).height() - $('#modal-wrapper').height()) / 2 + $('#modal-colba')[0].offsetTop)) {
+                $('#modal-ball').css('top', `${el.pageY - ($(window).height() - $('#modal-wrapper').height()) / 2 - $('#modal-text')[0].offsetTop - $('#modal-ball').height() / 2}px`);
+            }
+            if (($('#modal-ball')[0].offsetTop <= 30) && (check == true)) {
+                check = false;
+                ballDown = false;
+                $('#modal').hide();
+                $('html').css('overflow', $('html').data('previous-overflow'));
+            }
+            if (ballDown == false) {
+                $('#modal-ball').css('top', `${$('#modal-colba').height() - $('#modal-ball').height()}px`);
+            }
+        });
+    }
+});
