623da2ddc1a84c51bf13ed683b118f7cf51421b7
reza--boy <60574856+danelloptz@users.noreply.github.com>
2022-11-18 21:40:19 +0300

    Add files via upload

--- kes/bulls.js
+++ kes/bulls.js
@@ -3,18 +3,36 @@ $('.btn--group--last').click(function() {
     $(location).attr('href','last_season.html');
 });
 $('.btn--group--curr').click(function() {
-    $(location).attr('href','curr_season.html');
+    $(location).attr('href','index.html');
 });
 
 // navigation
 $('.nav--stat').click(function() {
-    $('.games').css('display', 'none');
+    $('.games, .best, .seasons').css('display', 'none');
     $('.games--stat').css('display', 'flex');
+    $('.nav--games, .nav--players, .nav--seasons').removeClass('active');
+    $(this).addClass('active');
 });
 
 $('.nav--games').click(function() {
     $('.games').css('display', 'flex');
-    $('.games--stat').css('display', 'none');
+    $('.games--stat, .best, .seasons').css('display', 'none');
+    $('.nav--stat, .nav--players, .nav--seasons').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.nav--players').click(function() {
+    $('.best').css('display', 'block');
+    $('.games--stat, .games, .seasons').css('display', 'none');
+    $('.nav--games, .nav--stat, .nav--seasons').removeClass('active');
+    $(this).addClass('active');
+});
+
+$('.nav--seasons').click(function() {
+    $('.seasons').css('display', 'block');
+    $('.games--stat, .games, .best').css('display', 'none');
+    $('.nav--games, .nav--stat, .nav--players').removeClass('active');
+    $(this).addClass('active');
 });
 
 // modal stat
@@ -23,9 +41,14 @@ $('.games--stat--wrapper--line').click(function(el) {
     let modal = $(this).attr('aria-value');
     let id = $(this).attr('aria-name');
     let file_name;
-    if ($(location)[0].pathname.indexOf('last') != -1) { file_name = 'last'; }
-    else { file_name = 'curr'; }
-    $(location).attr('href',`${file_name}_season.html#${id}`);
+    if ($(location)[0].pathname.indexOf('last') != -1) { 
+        file_name = 'last';
+        $(location).attr('href',`${file_name}_season.html#${id}`); 
+    }
+    else { 
+        file_name = 'index'; 
+        $(location).attr('href',`${file_name}.html#${id}`);
+    }
     setTimeout(() => {
         $('html').data('scroll-position', $(this).offset().top).data('previous-overflow', $('html').css('overflow')).css('overflow', 'hidden'); // выключаем прокрутку
     }, 500);
@@ -63,12 +86,15 @@ $('.wrapper--card').click(function() {
     $('.games').css('display', 'none');
     $('.games--stat').css('display', 'flex');
     if ($(location)[0].pathname.indexOf('last') != -1) { 
-        file_name = 'last'; 
+        file_name = 'last';
+        $(location).attr('href',`${file_name}_season.html#${match_name}`);
     }
     else { 
-        file_name = 'curr'; 
+        file_name = 'index'; 
+        $(location).attr('href',`${file_name}.html#${match_name}`);
     }
-    $(location).attr('href',`${file_name}_season.html#${match_name}`);
+    $('.nav--games, .nav--players, .nav--seasons').removeClass('active');
+    $('.nav--stat').addClass('active');
     $(`.games--stat--wrapper:eq(${number})`).css('display', 'flex');
     $(`.games--stat--date:eq(${number})`).attr('aria-checked', '1').text('Закрыть статистику');
 });
